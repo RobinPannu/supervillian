e4617fd5a3736df3b3851eb7130efe00c3ef373d
Robin <pannusaab1997@gmail.com>
2017-07-13 22:21:07 -0400

    Added IHasMalice interface

---
+++ COMP123-S2017-Lesson9A/IHasMalice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Robin singh
+ * Date: July 13, 2017
+ * Description: This is an interface that defines a Malice property that 
+ * must be implement in any class that subscribes to it.
+ * Version: 0.1 - Created IHasMalice interface
+ */
+namespace COMP123_S2017_Lesson9A
+{
+    /// <summary>
+    /// this is the IHasMalice interface 
+    /// </summary>
+    public interface IHasMalice
+    {
+        
+
+        // PUBLIC PROPERTIES
+        int Malice { get; set; }
+    }
+}
