81de236fa59142f968afafefa77a669608f5efd7
Drithi <Drithi@DESKTOP-Q3DH4RM>
2017-07-13 21:40:43 -0400

    Add project files.

---
+++ COMP123-S2017-Lesson9A/SuperVillian.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2017_Lesson9A
+{
+    public class Supervillian : Human
+    {
+        // PRIVATE FIELDS
+        private List<Power> _powers;
+        public List<Power> Powers
+        {
+            get
+            {
+                return this._powers; // returns a reference to the Powers List
+            }
+        }
+        class SuperVillian
+    {
+    }
+}
