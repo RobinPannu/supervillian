c8605277caed028064852192d2d0c0d4a27725eb
Robin <pannusaab1997@gmail.com>
2017-07-13 22:34:47 -0400

    Added SuperVillain Class

---
+++ COMP123-S2017-Lesson9A/SuperVillian.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+ * Name: Robin singh
+ * Date: July 13, 2017
+ * Description: This is the SuperVillian class which inherits from the SuperHuman class
+ * and implement the IHasMalice Interface 
+ * Version: 0.1 - Created the SuperVillian class
+ */
+namespace COMP123_S2017_Lesson9A
+{
+    /// <summary>
+    /// this is the SuperVillian class
+    /// </summary>
+    public class SuperVillian : SuperHuman, IHasMalice
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private int _malice;
+
+
+        // PUBLIC PROPERTIES
+        public int Malice
+        {
+
+            get
+            {
+                return this._malice;
+
+            }
+            set
+            {
+                this._malice = value;
+            }
+        }
+
+
+        // CONSTRUCTORS
+        /// <summary>
+        /// This is the main constructor for the SuperVillian class
+        /// I t takes one argument - name (string)
+        /// </summary>
+        /// <param name="name"></param>
+        public SuperVillian(string name)
+            : base(name)
+        {
+
+        }
+        //PRIVATE METHODS 
+
+        // PUBLIC METHODS 
+    }
+}
